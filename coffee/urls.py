3bc8d15555f033af2bc877a1940d1d5d73f7d620
Ivy Nyawira <ivynyawira436@gmail.com>
2025-05-01 20:17:53 +0300

    Coffee Yield Predictor version 1

---
+++ coffee/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path # type: ignore
+from . import views
+from coffee.views import download_report, send_feedback
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('register/', views.register_user, name='register'),
+    path('login/', views.login_user, name='login'),
+    path('logout/', views.logout_user, name='logout'),
+    path('predictions/', views.predictions, name='predictions'),
+    path('success/', views.success, name='success'),
+    path('results/', views.prediction_results, name='prediction_results'),
+    path('predict/', views.predict_yield, name='predict_yield'),
+    path('save-farm-info/', views.save_farm_info, name='save_farm_info'),
+    path('res/', views.submit_farm_info, name='submit_farm_info'),
+     path('download-report/', download_report, name='download_report'),
+    path('send-feedback/', send_feedback, name='send_feedback'),
+]
+
+
+
+
